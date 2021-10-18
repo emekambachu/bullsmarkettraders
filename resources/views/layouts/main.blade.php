c1c27ae851d04df9aada68f68e8f2aa3946eb3b0
Emeka Victor Mbachu <victor.c.mbachu@gmail.com>
2021-10-18 13:29:49 +0100

    added tawk

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -254,6 +254,20 @@
 
 @yield('bottom-assets')
 
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/616d67d9f7c0440a591ec6cf/1fi9lraor';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
+
 </body>
 
 </html>
