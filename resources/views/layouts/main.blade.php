b9965668915fd758a5329d5257cc24821098469b
Emeka Victor Mbachu <32366820+emekambachu@users.noreply.github.com>
2022-04-16 22:12:45 +0100

    update

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -22,14 +22,14 @@
 <body>
 
 <!-- preloader area start -->
-<div class="preloader" id="preloader">
-    <div class="preloader-inner">
-        <div class="spinner">
-            <div class="dot1"></div>
-            <div class="dot2"></div>
-        </div>
-    </div>
-</div>
+{{--<div class="preloader" id="preloader">--}}
+{{--    <div class="preloader-inner">--}}
+{{--        <div class="spinner">--}}
+{{--            <div class="dot1"></div>--}}
+{{--            <div class="dot2"></div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</div>--}}
 <div class="body-overlay" id="body-overlay"></div>
 <!-- preloader area end -->
 
